d729f3c2034ad2b4dc9899a71c806461705de5f9
ittp <61617660+ittp@users.noreply.github.com>
2022-02-25 19:56:34 +0300

    Create menu.sh

---
+++ function manageMenu() { 	echo "Welcome to OpenVPN-install!" 	echo "The git repository is available at: https:/github.com/angristan/menu.sh
@@ -0,0 +1,51 @@
+
+functions() {
+  # create user
+  # curl -X POST https://api.ittps.ru/openvpn/create/user/$query
+  # edit user
+  # delete user
+
+}
+
+function init_menu() {
+
+} 
+
+# create_user username config
+create_user() {
+  local $username=$1, $config=$2
+  echo "ovpn create user <username> <config>"
+  
+  
+}
+
+function users_menu() {
+	echo "OpenVPN Users managment"
+  echo "####################################################"
+	echo "   a) Add a new user"
+  echo "   e) Edit new user"
+	echo "   2) Revoke existing user"
+	echo "   3) Remove OpenVPN"
+	echo "   4) Exit"
+  
+    echo "####################################################"
+
+	until [[ $MENU_OPTION =~ ^[1-4]$ ]]; do
+		read -rp "Select an option [1-4]: " MENU_OPTION
+	done
+
+	case $MENU_OPTION in
+	1)
+# 		newClient
+		;;
+	2)
+# 		revokeClient
+		;;
+	3)
+# 		removeOpenVPN
+		;;
+	4)
+		exit 0
+		;;
+	esac
+}
